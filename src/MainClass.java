9e15a947def7afa24977d98c8a541ee1926fb9f5
Blank <godwinjay735@gmail.com>
2026-03-31 09:32:25 +0800

    Fixed some output

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,6 @@
 public class MainClass {
     public static void main(String[] args) {
-        Internsync system = new Internsync("InternSync", "v1.0");
+        Internsync system = new Internsync("InternSync", "v2.0");
         system.startSystem();
     }
 }
