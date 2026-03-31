6c5e387fb690b42ffbf4893688b384aaca3a6108
Blank <godwinjay735@gmail.com>
2026-03-31 14:51:54 +0800

    added an admin access

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,9 @@
 public class MainClass {
     public static void main(String[] args) {
+        // Default admin instance object
+        Admin admin = new Admin(1, "admin", "admin123", "admin@internsync.com");
+
+        // Initialize the system
         Internsync system = new Internsync("InternSync", "v3.0");
         system.startSystem();
     }
